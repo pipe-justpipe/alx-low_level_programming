c0ef81ce011eaf4608bd8e0bdb1125da75aa7970
pipe-justpipe <peculiarworth@gmail.com>
2022-12-11 18:54:47 -0800

    Task 3, Task 4, Task5 and Task 6

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: prints
+ *
+ * Return: Always (0)
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+
+}
---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+
+/**
+ * main - Print a script
+ *
+ * Description: Use printf
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	printf("%s", "with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+
+}
